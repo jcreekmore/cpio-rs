53f997a0fc07df4a4caf92488edec77d4ab28df8
Jonathan Creekmore <jcreekmore@users.noreply.github.com>
2023-07-11 13:26:18 -0400

    add a very simple write example

---
+++ examples/createcpio.rs
@@ -0,0 +1,24 @@
+// Create a CPIO archive from filenames passed through stdin.
+
+use cpio::{NewcBuilder, write_cpio};
+use std::fs::File;
+use std::io::{BufRead, BufReader, stdin, stdout};
+
+fn load_file(path: &str) -> io::Result<(NewcBuilder, File)> {
+	let builder = NewcBuilder::new(path)
+		.uid(1000)
+		.gid(1000)
+		.mode(0o100644);
+		
+	File::open(path)
+		.map(|fp| (builder, fp))
+}
+
+fn main() {
+	let inputs = stdin()
+		.lock()
+		.lines()
+		.map(|path| load_file(path.unwrap()).unwrap());
+		
+	write_cpio(inputs, stdout()).unwrap();
+}
